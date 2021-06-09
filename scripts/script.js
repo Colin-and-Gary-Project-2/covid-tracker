851c78f8a006d67a9f35ad07ff812c33305f6906
ColinCaldwell <colindcaldwell@gmail.com>
2021-06-08 23:39:05 -0400

    working on JS features

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+// const covidVaccinationApp = {};
+
+// covidVaccinationApp.apiCall{
+// const apiURL = 'https://api.covid19tracker.ca/summary/split/'
+// const url = new URL('http://proxy.hackeryou.com');
+//         url.search = new URLSearchParams({
+//         reqUrl: apiURL
+//         });
+
+// fetch(url)
+// .then(function(response){
+//     return response.json();
+// })
+// .then(function(jsonResult){
+//     console.log('it worked!', jsonResult)
+// })
+
+
+
+
+// Create namespace object
+// create async function to call API (at least twice, once for province data, once for covid data (summary/split))
+// create a function to append province names to dropdown
+// create a function to 
+
+// Before thursday: 
+// build java script functions
+// beautiful styling
+
+// Thursday meeting goals: Have our working MVP, review each other's code.
+
+// Stretch goals:
+// Form that dynamically appears as user makes selections
+// Data visualization, and extrapolation (graphs, and things like "X days till Ontario is fully vaxxed)
