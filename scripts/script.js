eb787ade9fdb89c723a8f44d2467ad6e383865f3
ColinCaldwell <colindcaldwell@gmail.com>
2021-06-11 22:40:15 -0400

    fixed SCSS, added JS to call and loop data

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,47 @@
-// const covidVaccinationApp = {};
+const App = {};
 
-// covidVaccinationApp.apiCall{
-// const apiURL = 'https://api.covid19tracker.ca/summary/split/'
-// const url = new URL('http://proxy.hackeryou.com');
-//         url.search = new URLSearchParams({
-//         reqUrl: apiURL
-//         });
+App.init = function(){
+    App.apiCall();
+    App.showData();
+}
 
-// fetch(url)
-// .then(function(response){
-//     return response.json();
-// })
-// .then(function(jsonResult){
-//     console.log('it worked!', jsonResult)
-// })
+App.apiCall = function(){
+    // Retrieve API data
+    async function getAPIData(){
+        const apiURL = 'https://api.covid19tracker.ca/summary/split/'
+        const url = new URL('http://proxy.hackeryou.com');
+                    url.search = new URLSearchParams({
+                    reqUrl: apiURL
+                    });
+        const response = await fetch(url);
+        if (response){
+            const jsonData = await response.json();
+            // App.showData(jsonData)
+            return jsonData;
+        } else {
+            console.log(error);
+        }
+    }
+    // Promise to grab API data
+    const apiPromise = getAPIData();
+    apiPromise.then((data) => {
+        console.log(data);
+        App.showData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+}
 
+App.showData = function(data){
+    const loop = data.data.forEach((province) => {
+        console.log(province.change_hospitalizations)
+    });
+    console.log(loop);
+}
+
+
+App.init();
 
 
 
@@ -32,3 +59,20 @@
 // Stretch goals:
 // Form that dynamically appears as user makes selections
 // Data visualization, and extrapolation (graphs, and things like "X days till Ontario is fully vaxxed)
+
+
+// App.testCall = function(){
+    //     const apiURL = 'https://api.covid19tracker.ca/summary/split/'
+    //     const url = new URL('http://proxy.hackeryou.com');
+    //             url.search = new URLSearchParams({
+    //             reqUrl: apiURL
+    //             });
+    
+    //     fetch(url)
+    //     .then(function(response){
+    //         return response.json();
+    //     })
+    //     .then(function(jsonResult){
+    //         console.log('it worked!', jsonResult)
+    //     })
+    // };
